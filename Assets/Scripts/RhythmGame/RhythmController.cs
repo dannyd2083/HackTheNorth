89db555ec832b303238a6ac5228d47d03774b434
RageLeague <34314733+RageLeague@users.noreply.github.com>
2023-09-16 23:15:51 -0400

    Add anim

--- Assets/Scripts/RhythmGame/RhythmController.cs
+++ Assets/Scripts/RhythmGame/RhythmController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -56,11 +57,93 @@ public class RhythmController : MonoBehaviour
 
     public List<RhythmPattern> patterns;
     public float bpm = 118.0f;
+    public float yOffset = 288f;
+
+    public TextAsset easyPatternsFile;
+    public TextAsset mediumPatternsFile;
+    public TextAsset hardPatternsFile;
+
+    public RhythmMinigame minigame;
+
+    private float initialYPos = 0f;
+    private RectTransform rect;
+    private bool isRunning = false;
+    
+    public IEnumerator SlideIn(float totalTime)
+    {
+        Debug.Log("Sliding In");
+        float timePassed = 0f;
+        Vector2 newPos;
+        while (timePassed < totalTime)
+        {
+            yield return null;
+            timePassed += Time.deltaTime;
+            float tNorm = (totalTime - timePassed) / totalTime;
+            newPos = new Vector2(rect.anchoredPosition.x, initialYPos - yOffset * tNorm * tNorm);
+            rect.anchoredPosition = newPos;
+        }
+        newPos = new Vector2(rect.anchoredPosition.x, initialYPos);
+        rect.anchoredPosition = newPos;
+        Debug.Log("Sliding In Complete");
+    }
+    public IEnumerator SlideOut(float totalTime)
+    {
+        Debug.Log("Sliding Out");
+        float timePassed = 0f;
+        Vector2 newPos;
+        while (timePassed < totalTime)
+        {
+            yield return null;
+            timePassed += Time.deltaTime;
+            float tNorm = (timePassed) / totalTime;
+            newPos = new Vector2(rect.anchoredPosition.x, initialYPos - yOffset * tNorm * tNorm);
+            rect.anchoredPosition = newPos;
+        }
+        newPos = new Vector2(rect.anchoredPosition.x, initialYPos - yOffset);
+        rect.anchoredPosition = newPos;
+        Debug.Log("Sliding Out Complete");
+    }
+
+    public IEnumerator RhythmSequence (int difficulty)
+    {
+        if (isRunning) { yield break; }
+        isRunning = true;
+        string text = "";
+        switch (difficulty)
+        {
+            case 1:
+                text = easyPatternsFile.text;
+                break;
+            case 2:
+                text = mediumPatternsFile.text;
+                break;
+            case 3:
+                text = hardPatternsFile.text;
+                break;
+        }
+
+        AvailablePatterns patterns = JsonUtility.FromJson<AvailablePatterns>(text);
+        RhythmPattern chosen = patterns.patterns[UnityEngine.Random.Range(0, patterns.patterns.Count)];
+        minigame.ApplyRhythmPattern(chosen);
+
+        yield return SlideIn(0.2f);
+
+        RhythmScore score = new RhythmScore();
+
+        yield return minigame.PlayPattern(score);
+
+        yield return SlideOut(0.2f);
+        isRunning = false;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rect = GetComponent<RectTransform>();
+        initialYPos = rect.anchoredPosition.y;
+        rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, initialYPos - yOffset);
+
+        StartCoroutine(RhythmSequence(3));
     }
 
     // Update is called once per frame
